43520194e867beaf2563c4e06120f5d5820ecd8c
Hazel G <58535328+toaster203@users.noreply.github.com>
2024-09-28 11:25:27 -0400

    set up shadcn + tailwind

--- vite.config.js
+++ vite.config.js
@@ -11,18 +11,23 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
         rewrite: path => path.replace('/api', ''),
-        ws: true,
-          configure: (proxy, _options) => {
-            proxy.on('error', (err, _req, _res) => {
-              console.log('proxy error', err);
-            });
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              console.log('Sending Request to the Target:', req.method, req.url);
-            });
-            proxy.on('proxyRes', (proxyRes, req, _res) => {
-              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-            });
+        // ws: true,
+        //   configure: (proxy, _options) => {
+        //     proxy.on('error', (err, _req, _res) => {
+        //       console.log('proxy error', err);
+        //     });
+        //     proxy.on('proxyReq', (proxyReq, req, _res) => {
+        //       console.log('Sending Request to the Target:', req.method, req.url);
+        //     });
+        //     proxy.on('proxyRes', (proxyRes, req, _res) => {
+        //       console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+        //     });
+        // },
+        resolve: {
+          alias: {
+            "@": path.resolve(__dirname, "./src"),
           },
+        },
       }
     } 
   }

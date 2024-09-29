b3a95563a9a7e16b13d4222da4446d4eca5548e6
f24shen <f24shen@uwaterloo.ca>
2024-09-29 00:47:24 -0400

    added heading to landing and fridge pic

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { useRedirectFunctions } from '@propelauth/react';
 import { Button } from '@/components/ui/button';
+import closed from '../assets/closed.png'
 
 const LandingPage = () => {
   const { redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } = useRedirectFunctions()
   return (
-    <Button onClick={redirectToLoginPage}>open</Button>
+    <div className="flex h-full w-full">
+        <div className="w-1/2 flex justify-center items-center">
+          <div className="w-4/5">
+          <h1>Best BeFridged</h1>
+          <Button onClick={redirectToLoginPage}>open</Button>
+          </div>
+
+        </div>
+        <div className="w-1/2 flex justify-center items-center">
+          <img src={closed} alt="cute periwinkle fridge" className="w-3/5" />
+        </div>
+    </div>
   )
 }
 

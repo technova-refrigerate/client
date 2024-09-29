03f130f8b838e52e20a3f7e444fa0c6e01ae0cab
Sophia Yang <spiayanguni@gmail.com>
2024-09-29 01:14:01 -0400

    styling for stats page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import MainScreen from './pages/MainScreen';
 import LandingPage from './pages/LandingPage.jsx';
 import AddInventory from './pages/AddInventory.jsx';
-// import StatsPage from './pages/StatsPage.jsx'
+import StatsPage from './pages/StatsPage.jsx'
 import PropelLogin from './pages/PropelLogin.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 
@@ -15,7 +15,7 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/add" element={<AddInventory />} />
         <Route path="/login" element={<PropelLogin />} />
-        {/*<Route path="/statspage" element={<StatsPage />} />*/}
+        <Route path="/stats" element={<StatsPage />} />
         <Route path="/loginpage" element={<LoginPage />} /> {/*debug stuff*/}
       </Routes>
     </Router>

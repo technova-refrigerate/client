402c4d39f1ea9a315d5452bf0f0d19f125ddc87b
Nicole Cui <nicolexcui@gmail.com>
2024-09-29 04:41:46 -0400

    add aura + styling

--- src/pages/AddInventory.jsx
+++ src/pages/AddInventory.jsx
@@ -3,12 +3,19 @@ import InventoryManager from '../components/InventoryManager';
 import ProductManager from '../components/ProductManager';
 import open from '../assets/open.png'
 import alm from '../assets/foods/alm.png'
+import alm_aura from '../assets/foods-aura/alm_aura.png'
 import avo from '../assets/foods/avo.png'
+import avo_aura from '../assets/foods-aura/avo_aura.png'
 import butter from '../assets/foods/butter.png'
+import butter_aura from '../assets/foods-aura/butter_aura.png'
 import garlic from '../assets/foods/garlic.png'
+import garlic_aura from '../assets/foods-aura/garlic_aura.png'
 import nana from '../assets/foods/nana.png'
+import nana_aura from '../assets/foods-aura/nana_aura.png'
 import pow from '../assets/foods/pow.png'
+import pow_aura from '../assets/foods-aura/pow_aura.png'
 import steak from '../assets/foods/steak.png'
+import steak_aura from '../assets/foods-aura/steak_aura.png'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { withAuthInfo, useRedirectFunctions, useLogoutFunction } from '@propelauth/react'
 
@@ -40,12 +47,19 @@ const AddInventory = withAuthInfo((props) => {
         <div className="w-1/2 flex justify-center items-center">
           <img src={open} alt="cute periwinkle fridge" className="w-3/5" />
           <img src={alm} alt="almond milk" className="absolute h-20 mb-20 mr-36" />
+          {/*<img src={alm_aura} alt="almond milk" className="absolute h-24 mb-20 mr-36" />*/}
           <img src={avo} alt="avocado" className="absolute h-12 mb-56 mr-40" />
+          {/*<img src={avo_aura} alt="avocado" className="absolute h-14 mb-56 mr-40" />*/}
           <img src={butter} alt="butter" className="absolute h-12 mb-10 mr-16" />
+          {/*<img src={butter_aura} alt="butter" className="absolute h-14 mb-10 mr-16" />*/}
           <img src={garlic} alt="garlic" className="absolute h-12 mb-52 mr-20" />
+          {/*<img src={garlic_aura} alt="garlic" className="absolute h-16 mb-52 mr-16" />*/}
           <img src={nana} alt="bananas" className="absolute h-14 mb-56 ml-10" />
+          {/*<img src={nana_aura} alt="bananas" className="absolute h-20 mb-56 ml-10" />*/}
           <img src={pow} alt="powdered milk" className="absolute h-20 mb-12 ml-16" />
+          {/*<img src={pow_aura} alt="powdered milk" className="absolute h-20 mb-12 ml-16" />*/}
           <img src={steak} alt="avocado" className="absolute h-12 mt-28 ml-20" />
+          {/*<img src={steak_aura} alt="avocado" className="absolute h-14 mt-28 ml-20" />*/}
         </div>
       </div>
     )
